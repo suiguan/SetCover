756aa5638ca0f76ba4665b26028b5d356126996a
Sui Kun Guan <sui.kun.guan@gmail.com>
2016-11-11 23:53:24 -0800

    Added greedy algorithm to find the optimal interval cover problem

--- setcover.py
+++ setcover.py
@@ -1,6 +1,7 @@
 import sys
 import os
 import random
+import time
 
 class IntervalSetCoverGenerator:
 
@@ -31,10 +32,54 @@ class IntervalSetCoverGenerator:
       universe = (start, end)
       return (universe, subset)
 
+class IntervalSetCoverSolver:
 
+    def __init__(self, universeInterval, subIntervals):
+        self.I = universeInterval
+        self.S = sorted(subIntervals, key=self.__getStart__) #sort sub-interval by end point 
+
+    def __getStart__(self, subInterval):
+        return subInterval[0]
+
+    def __getEnd__(self, subInterval):
+        return subInterval[1]
+
+    #repeatly select the largest sub interval that covers the leftmost uncovered points
+    def greedy(self):
+        cover = []
+        uncoveredInterval = self.I
+        subsets = self.S
+        while uncoveredInterval[0] <= uncoveredInterval[1]: #there is still uncovered elements 
+            leftmost = uncoveredInterval[0]
+            largestS = None
+            numSetCovered = 0 #number of subset covered the leftmost
+            for s in subsets:
+                if s[0] > leftmost: break # self.S is sorted
+                if s[1] >= leftmost:
+                    numSetCovered += 1
+                    if largestS == None or s[1] > largestS[1]:
+                        largestS = s
+            if largestS == None:
+                raise Exception("No subset cover point %s" % leftmost)
+
+            cover.append(largestS)
+            uncoveredInterval = (largestS[1] + 1, uncoveredInterval[1])
+            subsets = subsets[numSetCovered:]
+
+        return cover
+
+    def RandomizedLinearProgrammingRelaxiation(self):
 
 
 if __name__ == '__main__':
    g = IntervalSetCoverGenerator()
-   (I, S) = g.generate(0, 100000, 1000)
-   print 'I = %s, S = %s' % (I, S)
+   (I, S) = g.generate(0, 1000000, 10000)
+   print 'Universe Interval = %s' % (I,)
+   print 'Number of Sub-intervals = %d' % len(S)
+
+   begin = time.time()
+   solver = IntervalSetCoverSolver(I, S)
+   c = solver.greedy()
+   end = time.time()
+   print 'greedy alogrithm find optimal cover size %d in %.3f secs' % (len(c), end - begin)
+
